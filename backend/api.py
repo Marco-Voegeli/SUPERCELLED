b86fa366cc401ad46cd145ccfba4635e75273a45
alexis <alexis.tabin@gmail.com>
2022-03-24 07:37:38 +0100

    Add TextBlob and naive Bayes classification

--- backend/api.py
+++ backend/api.py
@@ -4,6 +4,8 @@ import text2emotion as te
 from pydantic import BaseModel
 import json
 from profanity_check import predict
+from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
 
 
 class textMessage(BaseModel):
@@ -26,6 +28,19 @@ def get_emotions(data: textMessage):
     text = data.text
     emotions = te.get_emotion(text)
     profanity = predict([text])[0].item()
+    blob = TextBlob(text)
+    naiveBayesBlob = TextBlob(text, analyzer=NaiveBayesAnalyzer())
+
+    polarity = blob.sentiment.polarity
+    subjectivity = blob.sentiment.subjectivity
+
+    naiveBayesClassification = naiveBayesBlob.sentiment.classification
+    naiveBayesP_pos = naiveBayesBlob.sentiment.p_pos
+    naiveBayesP_neg = naiveBayesBlob.sentiment.p_neg
+
+
+    polarity = blob.sentiment.polarity
+    subjectivity = blob.sentiment.subjectivity
 
     max_value = 0
     best_emotion = ''
@@ -34,7 +49,14 @@ def get_emotions(data: textMessage):
         if value > max_value:
             max_value = value
             best_emotion = emotion
-    return {"emotion" : best_emotion,
-            "profanity" : profanity,
+    return {   
+            "emotion" : best_emotion,
             "value" : max_value,
-            "emotions": emotions}
+            "profanity" : profanity,
+            "polarity": polarity,
+            "subjectivity": subjectivity,
+            "naiveBayesClassification": naiveBayesClassification,
+            "naiveBayesP_pos": naiveBayesP_pos,
+            "naiveBayesP_neg": naiveBayesP_neg,
+            "emotions": emotions
+            }
