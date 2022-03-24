bb3e7631c58ca90e964d148f97f7b3fe9db2927a
alexis <alexis.tabin@gmail.com>
2022-03-24 05:01:50 +0100

    OMG L'API MARCHE

--- backend/api.py
+++ backend/api.py
@@ -1,7 +1,10 @@
-import json
 from fastapi import FastAPI, UploadFile, File, HTTPException, Response, status
 from fastapi.middleware.cors import CORSMiddleware
 import text2emotion as te
+from pydantic import BaseModel
+
+class textMessage(BaseModel):
+    text: str
 
 app = FastAPI(
     title="Our Python Backend",
@@ -16,7 +19,12 @@ app.add_middleware(
 )
 
 @app.post("/get_emotions")
-def get_emotions(data: json):
-    text = data['text']
+def get_emotions(data: textMessage):
+    print("prout")
+    text = data.text
     return te.get_emotion(text)
 
+text = "I want to break free"
+emotions = te.get_emotion(text)
+
+print(emotions)
