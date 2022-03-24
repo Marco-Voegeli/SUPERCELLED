fb04c7f732f72d3a75193dde4f585b8e6b797e79
Maeul <mael.clemence@protonmail.com>
2022-03-24 17:57:50 +0100

    ALED

--- backend/openapi_test.py
+++ backend/openapi_test.py
@@ -4,31 +4,30 @@ import csv
 from click import prompt
 import openai
 
-listemotion = ""
+listemotion = []
 
 with open('EmotionsList.csv') as csvfile: 
     reader = csv.reader(csvfile, delimiter=',')
     for word in reader:
-        listemotion += (word[0]) + ", "
+        listemotion.append(word)
         
 
-
-GEN_FEEL_B = "What are the feelings in the following conversation between A and B ?"
-GEN_FEEL_E = "Conversation's emotions:"
+print(listemotion)
+#GEN_FEEL_B = "What are the feelings in the following conversation between A and B ?"
+#GEN_FEEL_E = "Conversation's emotions:"
 # A_FEEL_B = "How is A feeling in the following conversation:"
-A_FEEL_B = "What are the feelings of A in the following conversation ?"
-A_FEEL_E = "A's two most prominent emotions:"
+#A_FEEL_B = "What are the feelings of A in the following conversation ?"
+#A_FEEL_E = "A's two most prominent emotions:"
 
-A_FEEL_EMOJI = "What would be the best emoji to describe A's most proeminent emotion?"
+#A_FEEL_EMOJI = "What would be the best emoji to describe A's most proeminent emotion?"
 
-B_FEEL_B = "What are the feelings of B in the following conversation ?"
+#B_FEEL_B = "What are the feelings of B in the following conversation ?"
 # B_FEEL_B = "How is B feeling in the following conversation:"
-B_FEEL_E = "B's two most prominent emotions:"
-B_FEEL_EMOJI = "What would be the best emoji to describe B's most proeminent emotion?"
+#B_FEEL_E = "B's two most prominent emotions:"
+#B_FEEL_EMOJI = "What would be the best emoji to describe B's most proeminent emotion?"
 
 A_FEEL_ONE = "Using only one single word, express A's emotion: "
 B_FEEL_ONE = "Using only one single word, express B's emotion: "
-C_FEEL_ONE = "Using only one single word, express C's emotion: "
 
 
 class Conversation:
@@ -37,73 +36,33 @@ class Conversation:
 
     def add_message(self, msg: str):
         self.messages.append(msg + "\n")
-
+        
     def clear(self):
         self.messages = []
-
-    def to_prompts(self):
-        prompts = []
-        conv = ""
-        for msg in self.messages:
-            conv += msg
-        prompts.append(GEN_FEEL_B + "\n" + conv + "\n" + GEN_FEEL_E)
-        prompts.append(A_FEEL_B + "\n" + conv + "\n" + A_FEEL_E)
-        prompts.append(B_FEEL_B + "\n" + conv + "\n" + B_FEEL_E)
         
-        return prompts
-        
-    def to_prompts_one(self):
-        prompts = []
-        conv = ""
-        for msg in self.messages:
-            conv += msg
-        prompts.append(A_FEEL_ONE + "\n" + conv)
-        prompts.append(B_FEEL_ONE + "\n" + conv)
-        prompts.append(C_FEEL_ONE + "\n" + conv)
-
-        for prompt in prompts:
-            print(prompt)
-        return prompts
-
-    def get_emotions(self):
-        prompts = self.to_prompts()
-        resp = []
-
-        for pr in prompts:
-            # print(pr)
-            # print("-"*10)
-            resp.append(openai.Completion.create(
-                engine="text-davinci-002",
-                prompt=pr,
-                temperature=0,
-                max_tokens=60,
-                top_p=1.0,
-                frequency_penalty=0.0,
-                presence_penalty=0.0)["choices"][0]["text"]
-            )
-
-        for res in resp:
-            print("*" * 10)
-            print(res)
-            print("-" * 10)
-        return res
 
     def get_emotions_oneWord(self):
-        prompts = self.to_prompts_one()
+        messages = self.messages
         resp = []
 
-        for pr in prompts:
+        for message in messages:
             # print(pr)
             # print("-"*10)
-            resp.append(openai.Completion.create(
-                engine="text-davinci-002",
-                prompt=pr,
-                temperature=0,
-                max_tokens=60,
-                top_p=1.0,
-                frequency_penalty=0.0,
-                presence_penalty=0.0)["choices"][0]["text"]
-            )
+            resp.append(openai.Classification.create(
+                search_model="ada",
+                model="curie",
+                query="I am tired",
+                labels=[["I am sad","sad",]
+    ["I feel awesome","happy",]
+    ["I did not expect that","suprise",]
+    ["It is boring","bored"]
+    ["It really scares me","fear",]
+    ["I want to punch someone","angry"]
+    ["I do not understand","confused"]
+    ["I do not want to speak about it","irritated"]
+    ["You really disappointed me","disappointed"]
+    ["I am exhausted","tired",]],
+            ))
         
         return resp    
 
@@ -113,15 +72,9 @@ openai.api_key = "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"
 c = Conversation()
 
 c.add_message("A: I am tired today, would you like to talk ?")
+
 ##c.get_emotions()
 answers = c.get_emotions_oneWord()
 
-toSend = []
-for answer in answers:
-    tmp = answer.replace('\n',"")
-    tmp = tmp.replace(' ', "")
-    tmp = tmp.replace('', "")
-
-    toSend.append(tmp[2:])
+print(answers)
     
-print(toSend)
