0a7846cfec76a8269e4e0bc0133bd7793d18b211
alexis <alexis.tabin@gmail.com>
2022-03-25 03:16:16 +0100

    GIF

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ import websockets
 import json
 from datetime import datetime
 from openapi_test2 import get_emotions
+from urllib import parse, request
 
 users = {
     '0': 'A',
@@ -12,6 +13,11 @@ users = {
 }
 clients = dict()
 msgs = []
+good_emotions = ['happy', 'friendly', 'flattered', 'admirative', 'amused', 'caring', 'desiring', 'excited', 'grateful', 'joyful', 'loved', 'optimist', 'proud', 'relieved']
+bad_emotions = ['insulted', 'embarrassed', 'sad', 'angry', 'disrepected', 'ashamed', 'shaken', 'unsafe', 'bullied', 'unconfortable', 'angry', 'annoyed', 'disappointed', 'disapproved', 'disgusted', 'afraid', 'griving', 'nervous', 'remorse', 'sad']
+neutral_emotions = ['confused', 'curious', 'realized', 'suprised']
+GIPHY_API_KEY = 'pui3355ayqU0UNdFY4Yt6IDiNOrgk2tn'
+GIPHY_URL = "http://api.giphy.com/v1/gifs/search"
 
 async def error(websocket, message):
     print(message)
@@ -61,12 +67,32 @@ async def send_msgs():
     for id in clients :
         await clients[id].send(json.dumps({"msgs" : msgs }))
     for id in clients :
-        await clients[id].send(json.dumps({"emotions": compute_emotions()}))
+        raw_data = compute_emotions()
+        a_feeling = raw_data[0].split()[-1]
+        b_feeling = raw_data[0].split()[-1]
+        a_gif_url = get_GIF_url(a_feeling)
+        b_gif_url = get_GIF_url(b_feeling)
+        await clients[id].send(json.dumps({"emotions": raw_data, "A_gif_url": a_gif_url, "B_gif_url": b_gif_url}))
 
 async def main():
     async with websockets.serve(handler, "0.0.0.0", 8002):
         await asyncio.Future()  # run forever
 
+def get_GIF_url(feeling):
+    params = parse.urlencode({
+    "q": feeling,
+    "api_key": GIPHY_API_KEY,
+    "limit": "1"
+    })
+
+    with request.urlopen("".join((GIPHY_URL, "?", params))) as response:
+        data = json.loads(response.read())
+
+    #coucou = json.dumps(data, sort_keys=True, indent=4)
+    coucou = data['data']
+    first = coucou[0]
+    url = first['url']
+    return url
 
 def compute_emotions():
     # msg = text, userid, timestamp
